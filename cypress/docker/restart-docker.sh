9b87e860de942328468af55f5c74ff02e703d9d5
guocuijian1 <guocuijian1979@gmail.com>
2025-08-27 20:23:50 +0800

    1. update shell logic 2. add git hub action config file

--- cypress/docker/restart-docker.sh
+++ cypress/docker/restart-docker.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-docker-compose down -v
-docker-compose up -d
+# 获取当前脚本所在目录（不使用cd，直接用BASH_SOURCE）
+SCRIPT_DIR="$(dirname ${BASH_SOURCE[0]})"
+# 计算 docker-compose.yml 的绝对路径
+YML_PATH="$SCRIPT_DIR/docker-compose.yml"
 
+docker-compose -f "$YML_PATH" down -v
+docker-compose -f "$YML_PATH" up -d
